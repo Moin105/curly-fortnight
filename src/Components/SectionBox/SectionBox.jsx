8e7428eb467e35ba208ef9ff84acb6a6fdf3044a
moin105 <moinlatif105@gmail.com>
2023-10-17 05:12:06 +0500

    section page

---
+++ src/Components/SectionBox/SectionBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+function SectionBox({title}) {
+  return (
+    <div className='section-box'>
+        <span>edit</span>
+        <h2>{title}</h2>
+    </div>
+  )
+}
+
+export default SectionBox
--- src/Pages/Sections/Sections.jsx
+++ src/Pages/Sections/Sections.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './styles.css'
 import Layout from '../../Layout/Layout'
+import SectionBox from '../../Components/SectionBox/SectionBox'
 function Sections() {
   return (
     <div>
         <Layout>
             <div className='sections-container'>
-            <h1>Sections</h1>
+            <h2>Sections</h2>
+            <div className='section-wrapper'>
+               <SectionBox title='Section1' />
+               <SectionBox title='Section2' />
+               <SectionBox title='Section3' />
+               
+            </div>
             </div>
         </Layout>
     </div>
